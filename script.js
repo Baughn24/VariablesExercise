6ccf2bd5d9d9f134c5e340db4f5ef74c953d433d
Allie Baughn <allison.baughn@gmail.com>
2021-09-21 14:40:41 -0500

    Completed variables exercise

---
+++ script.js
@@ -0,0 +1,37 @@
+// 1
+// const
+
+// 2
+let num = 20;
+console.log(num);
+// 3
+num = 15;
+console.log(num);
+// 4
+num += 7;
+console.log(num);
+// 5
+num--
+console.log(num);
+// 6
+num = "This is not a number"
+console.log(num);
+
+// 7
+let blank;
+// 8
+console.log(blank);
+// 9
+blank = null;
+console.log(blank);
+
+// BONUS 1
+const [quarter, dime, nickle, penny] = [0.25, 0.10, 0.05, 0.01];
+// BONUS 2
+console.log(quarter, dime, nickle, penny );
+
+
+// BONUS 3
+let letters = "qwertyuiop"
+// BONUS 4
+console.log(letters[9]);
